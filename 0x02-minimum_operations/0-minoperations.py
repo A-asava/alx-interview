8e38cccb53b7ccab8adf76d46f7de47134bfdc8c
A-asava <adikasaid996@gmail.com>
2024-08-19 15:44:17 +0000

    minimum_operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""Minimum Operations"""
+
+
+def minOperations(n):
+    """method that calculate the
+    fewest number of operations needed
+    to result in exactly n H characters
+    in the file.
+    """
+
+    current_len = 1
+    clipboard = 0
+    num_operations = 0
+
+    while current_len < n:
+        if clipboard == 0:
+            # copyall
+            clipboard = current_len
+            # increment operations counter
+            num_operations += 1
+
+        # if haven't pasted anything yet
+        if current_len == 1:
+            # paste
+            current_len += clipboard
+            # increment operations counter
+            num_operations += 1
+            # continue to next loop
+            continue
+
+        remaining = n - current_len
+        if remaining < clipboard:
+            return 0
+
+        if remaining % current_len != 0:
+            current_len += clipboard
+            num_operations += 1
+        else:
+            clipboard = current_len
+            current_len += clipboard
+            num_operations += 2
+    if current_len == n:
+        return num_operations
+    else:
+        return 0
